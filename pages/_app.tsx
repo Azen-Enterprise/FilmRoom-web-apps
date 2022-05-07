d186582faa8565af42e1be5dd5cc0bd0b2f6dac1
Daniel Carlson <dancarl758@gmail.com>
2022-05-07 22:49:49 +0100

    [feat]: completed home page for web

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
