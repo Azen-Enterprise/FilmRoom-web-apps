9e9ad2590c342c9b6572319935c04b30839622be
larrytech7 <larryakah@gmail.com>
2022-05-12 00:28:38 +0100

    Updated content for the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.mainContainer}>
           <div>
-            <h1 className={styles.title}>All Your Greatest Shows</h1>
-            <h4 className={styles.subTitle}>In one place with great plans starting at just 2000 XAF/month</h4>
+            <h1 className={styles.title}>Diversity at its best</h1>
+            <h4 className={styles.subTitle}>The best of culture in one place. Here lies the birth place of humanity.</h4>
             <br /><br /><br /><br />
             <p>
-              <span className={styles.slogan}>BUILT FOR AFRICANS, BY AFRICANS</span>
+              <span className={styles.slogan}>BUILT FOR THE WORLD, BY AFRICANS</span>
             </p>
             <p>
               <button className={styles.btn}>Get Started</button>
@@ -30,30 +30,35 @@ const Home: NextPage = () => {
 
       {/* Features */}
       <section className={styles.featuresContainer}>
-        <h3 className={styles.sectionTitle}>Features</h3>
-        <h5 className={styles.featuresSubTitle}>Built just for you with your needs at heart</h5>
+        <h3 className={styles.sectionTitle}>Our Offer</h3>
+        <h5 className={styles.featuresSubTitle}>We are here to honor and preserve the great African culture and heritage</h5>
         <div className={styles.cardContainer}>
           <div className={styles.card}>
             <Image src="/images/hd-icon.png" height={50} width={50} alt="depiction of HD logo"/>
-            <h3>Buy & Watch High Definition Movies</h3>
-            <p>You can watch very good movies in HD and enjoy every moment</p>
+            <h3>Buy & Watch High Definition Movies anywhere, anytime</h3>
+            <p>You can watch or stream movies in HD and enjoy every moment. We provide a plethora of content that honors great culture.</p>
           </div>
           <div className={styles.card}>
             <Image src="/images/family-icon.png" height={40} width={40} alt="depiction of a family" />
-            <h3>Share the fun with your family members</h3>
-            <p>You can watch very good movies in HD and enjoy every moment</p>
+            <h3>Share and earn with family and friends</h3>
+            <p>For every person you refer to the platform, earn a comission that gives you the ability to purchase even more content.</p>
+          </div>
+          <div className={styles.card}>
+            <Image src="/images/family-icon.png" height={40} width={40} alt="depiction of a family" />
+            <h3>Gift your family or friends with movie tokens</h3>
+            <p>Purchase movies on behalf of your family or friends so they can enjoy the great content that you enjoy.</p>
           </div>
           <div className={styles.card}>
             <Image src="/images/laptop-icon.png" height={40} width={40} alt="depiction of laptop" />
-            <h3>Get the best right on your devices</h3>
-            <p>You can watch very good movies in HD and enjoy every moment</p>
+            <h3>Great producers deserve a fair reward.</h3>
+            <p>We support great producers by ensuring that their content reaches the maximum audience possible at a fraction of the market cost.</p>
           </div>
         </div>
       </section>
 
       {/* Top Rated Movies */}
       <section className={styles.featuresContainer}>
-        <h3 className={styles.sectionTitle}>Top Rated Movies</h3>
+        <h3 className={styles.sectionTitle}>Trending</h3>
         <br /><br />
         <div className={styles.cardContainer}>
           <div className={styles.movieCard1}></div>
@@ -66,7 +71,7 @@ const Home: NextPage = () => {
       <section className={styles.cta}>
         <div className={styles.contentContainer}>
           <h2>Get Our Apps on All your devices</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuriesLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+          <p>We  make it a mission to bring the platform to all your devices from smarphone to your living room cinema. Get started with our smartphone applications on your favorite stores.</p>
           <Image src="/images/playstore-logo.png" alt="Google play store" height={110} width={380} />
           <Image src="/images/app-store-logo.png" alt="App store" height={100} width={300} />
         </div>
@@ -80,7 +85,7 @@ const Home: NextPage = () => {
           <span className={styles.logo}>
             <Image src="/images/logos/footer-logo.png" alt="Film room logo" height={25} width={25} />
           </span>
-          <span>Filmroom &copy; Copyright 2022</span>
+          <span>FilmRoom &copy; Copyright 2022</span>
         </div>
         <div className={styles.footerContent}>
           <span>Privay Policy</span>
