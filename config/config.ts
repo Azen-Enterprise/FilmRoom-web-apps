ad28191f0d3c1139e884c262057aaebe6a2d1533
Daniel Carlson <dancarl758@gmail.com>
2022-05-15 17:01:57 +0100

    hotfix: updated firebase config info

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCNMK1xPsCJEPzkopiDV7YAE3jbYPJZuDI",
-  authDomain: "filmroom-9a782.firebaseapp.com",
-  projectId: "filmroom-9a782",
-  storageBucket: "filmroom-9a782.appspot.com",
-  messagingSenderId: "487143352829",
-  appId: "1:487143352829:web:9f281b5ff7618c51a05194",
-  measurementId: "G-EQPMLZ7VYR"
+  apiKey: "AIzaSyDrTH5nyqbtVyNN7s-J87Vr2ZJ0JyC0KoY",
+  authDomain: "filmroom-fffd3.firebaseapp.com",
+  databaseURL: "https://filmroom-fffd3-default-rtdb.firebaseio.com",
+  projectId: "filmroom-fffd3",
+  storageBucket: "filmroom-fffd3.appspot.com",
+  messagingSenderId: "1031326350905",
+  appId: "1:1031326350905:web:adf505b76c9312a0ea17e8",
+  measurementId: "G-BCMZV0BM3H"
 };
 
 export default firebaseConfig;
